6bed48b1f8ca8d8afffb269c44a59127ba5fafc6
root <root@ip-172-31-64-211.ec2.internal>
2024-03-15 11:53:44 +0000

    sg udpate

--- openzfs.tf
+++ openzfs.tf
@@ -5,7 +5,17 @@ resource "aws_fsx_openzfs_file_system" "wp" {
   security_group_ids =  [module.eks.node_security_group_id]
   deployment_type     = "MULTI_AZ_1"
   throughput_capacity = 160
+  depends_on = [ module.eks ]
 }
+
+# Adding security group rule for nodes to communicate with openzfs
+
+resource "aws_vpc_security_group_ingress_rule" "allow_vpc_traffic_ipv4" {
+  security_group_id = module.eks.node_security_group_id
+  cidr_ipv4         = var.vpc_cidr
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 output "openfsx-volume" {
 	description = "FileSystem ID to be consumed with K8s resources"
